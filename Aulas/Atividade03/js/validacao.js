ae9ae1d0439f9ecb285eeeb8d031837d3d43636e
rayssaetec <seuemail@example.com>
2025-05-06 11:20:12 -0300

    ok

--- Aulas/Atividade03/js/validacao.js
+++ Aulas/Atividade03/js/validacao.js
@@ -1,38 +1,48 @@
-//Validação simples
-//$("#formExemplo").validate();
-
 $("#formExemplo").validate({
     debug: true,
-    rules:{
-        txtemail:{
-            required: true, 
+    rules: {
+        txtemail: {
+            required: true,
             email: true,
         },
         txtnome: {
             required: true,
             rangelength: [2, 50],
         },
-        txtidade: {
-            min: 18,
-            max: 100,
-            required
-            //range: [18, 100]
-        }
+        txtfilho: {
+            required: true,
+            min: 0,
+            max: 10,
+        },
+        txtadmissao: {
+            required: true,
+            date: true,
+        },
+        txturl: {
+            required: true,
+            url: true,
+        },
     },
-    messages:{
-        txtemail:{
+    messages: {
+        txtemail: {
             required: "E-mail obrigatório",
             email: "Formato inválido do e-mail",
         },
         txtnome: {
-            required: "Idade obrigatório",
-            minlength: "Exigido 2 caracteres",
-            maxlength: "Até 50 caracteres",
+            required: "Nome obrigatório",
+            rangelength: "Nome deve ter entre 2 e 50 caracteres",
         },
-        txtidade: {
-            required: "Idade obrigatório",
-            min: "Valor mínimo: 18",
-            max: "Valor máximo: 50",
-        }
+        txtfilho: {
+            required: "Nº de filhos obrigatório",
+            min: "Valor mínimo: 0",
+            max: "Valor máximo: 10",
+        },
+        txtadmissao: {
+            required: "Data de Admissão obrigatória",
+        },
+        txturl: {
+            required: "URL obrigatória",
+            url: "Formato inválido da URL",
+        },
     }
 });
